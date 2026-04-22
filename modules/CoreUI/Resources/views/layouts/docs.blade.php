812ae71e733975f381fd008e3973deff9a836c44
Mark Gregory <mark.gregory@mpba.co.uk>
2026-04-22 12:04:38 +0100

    new design

--- modules/CoreUI/Resources/views/layouts/docs.blade.php
+++ modules/CoreUI/Resources/views/layouts/docs.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>@yield('title', config('app.name') . ' Documentation')</title>
+	<title>@yield('title', config('app.name') . '')</title>
 
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -25,11 +25,7 @@
 				<div class="flex h-10 w-10 items-center justify-center rounded-xl bg-white/10 text-lg font-bold text-white">
 					D
 				</div>
-
 				<div class="sidebar-brand-text">
-					<div class="text-sm font-medium uppercase tracking-[0.2em] text-sky-200">
-						Documentation
-					</div>
 					<div class="text-lg font-bold text-white">
 						@yield('brand_title', config('app.name'))
 					</div>
